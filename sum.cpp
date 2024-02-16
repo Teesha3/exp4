585c2b79f389e3e00ca135fb2ab54a67464f3b19
Teesha3 <157092368+Teesha3@users.noreply.github.com>
2024-02-16 10:35:18 +0530

    Create sum.cpp

---
+++ sum.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+ using namespace std;
+
+ int main(){
+    cout<<"plus"<<endl;
+    int a,b,c;
+
+    cout<<"enter 2 nums"<<endl;
+   cin>>a>>b;
+   c=a+b;
+   cout<<c<<endl;
+    return 0;
+ }
